26b3dbd3a23a2032520a4b4a19b89a87cbcbaf4a
Malope77 <nkadimenggivenmashego@gmail.com>
2026-03-18 15:14:48 +0200

    Update test.java

--- test.java
+++ test.java
@@ -13,3 +13,23 @@ public class TryHeapSort {
   Heap(int capacity) {
     array = new String[capacity];
     size = 0;
+    comparisons = 0
+      swaps = 0;
+  }
+
+  void buildTopDown(String[] words) {
+    array = new String[words.length];
+    size = 0;
+    comparisons = 0;
+    swaps = 0;
+
+    for (String word : words) {
+      insert(word)
+    }
+    System.out.printIn ("Top down heap built with " + size + "words");
+
+    void insert(String word) {
+      array[size] = words:
+    
+    
+  

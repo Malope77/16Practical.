524969a4ef20f992628dd7494b78d83699e0f608
Malope77 <nkadimenggivenmashego@gmail.com>
2026-03-18 15:04:42 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,15 @@
 Start Heapsort Project
+
+import java.io.*;
+import java.util.*;
+
+public class TryHeapSort {
+  private static class Heap {
+    String[] array;
+    int size;
+    long comparisons;
+    long swaps;
+
+  Heap(int capacity) {
+    array = new String[capacity];
+    size = 0;
